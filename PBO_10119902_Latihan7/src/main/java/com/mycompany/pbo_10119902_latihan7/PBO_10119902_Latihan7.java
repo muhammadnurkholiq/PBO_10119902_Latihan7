03fe2e68b6fee9fe6c797ffe5ef70f5ced5e7769
muhammadnurkholiq <73203850+muhammadnurkholiq@users.noreply.github.com>
2020-10-21 10:36:53 +0700

    Add files via upload
    First Commit

---
+++ PBO_10119902_Latihan7/src/main/java/com/mycompany/pbo_10119902_latihan7/PBO_10119902_Latihan7.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.pbo_10119902_latihan7;
+
+/**
+ *
+ * @author Muhammad Nurkholiq
+ */
+public class PBO_10119902_Latihan7 {
+     public static void main(String[]args){
+        byte b;
+        short s;
+        int i;
+        long l;
+
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+
+        System.out.println("byte :" + b);
+        System.out.println("short :" + s);
+        System.out.println("int :" + i);
+        System.out.println("long :" + l);
+        }
+    
+}
